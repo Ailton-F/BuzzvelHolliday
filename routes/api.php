b5d182343dfe1fc9c10bce329b0ec1a067cda111
Ailton-F <ailtonxdz@gmail.com>
2024-03-13 18:47:07 -0300

    hotfix docker

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,9 @@ Route::middleware('auth:sanctum')->prefix('users')->group(base_path('routes/api/
 Route::middleware('auth:sanctum')->prefix('holiday-plans')->group(base_path('routes/api/holidayPlansRoutes.php'));
 Route::prefix('auth')->group(base_path('routes/api/authRoutes.php'));
 
+Route::get('/', function (){
+   return response()->json([
+       "message"=>"connection was successful"
+   ]);
+});
+
